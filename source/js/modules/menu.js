609c8f091b9a0bc4814a510e59f5a8453c0fade9
Alexander Svistunov <Svistunov.A.Ev@sberbank.ru>
2020-05-06 16:01:12 +0300

    add fill animation for story screen

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -16,10 +16,25 @@ export default () => {
   }
 
   for (let i = 0; i < menuLinks.length; i++) {
-    menuLinks[i].addEventListener(`click`, function () {
+    menuLinks[i].addEventListener(`click`, function (evt) {
       if (window.innerWidth < 1025) {
         header.classList.remove(`page-header--menu-opened`);
         document.body.classList.remove(`menu-opened`);
+      }
+
+      const isCurrentMenuLinkPrizes = menuLinks[i].getAttribute(`href`) === `#prizes`;
+      const isCurrentLocationStory = window.location.toString().includes(`#story`);
+
+      if (isCurrentMenuLinkPrizes && isCurrentLocationStory) {
+        evt.preventDefault();
+        let storyBackground = document.querySelector(`.js-story-background`);
+        storyBackground.classList.remove(`end-animation`);
+        storyBackground.classList.add(`start-animation`);
+        storyBackground.addEventListener(`animationend`, function () {
+          window.location = menuLinks[i].getAttribute(`href`);
+          storyBackground.classList.remove(`start-animation`);
+          storyBackground.classList.add(`end-animation`);
+        });
       }
     });
   }
